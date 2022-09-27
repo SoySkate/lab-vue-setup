9b1d7e6329a0353f80163bdcbddc489d5dc2ee52
Andrew <andreumartintorres.amt@gmail.com>
2022-09-27 11:35:32 +0200

    firstProjectsInVue

---
+++ project1-vue/src/components/HelloTest.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>AndrewMcAndrew</h1>
+  <div>{{ name }}</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let name = ref(`HOLAAAAAAAAAAANDREW`);
+</script>
+
+<style scoped></style>
